ad885ee9895bf47ba2c36eeb1c02d6036a0463b0
younggam <nainjoo2001@naver.com>
2023-06-20 17:27:24 +0900

    대강 구 움직임 완료

---
+++ Assets/Scripts/Piece.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Piece : MonoBehaviour
+{
+    public UnityEvent onSelect;
+    public UnityEvent onDeselect;
+    public UnityEvent onClicked;
+}
--- Assets/Scripts/Pieces.cs
+++ Assets/Scripts/Pieces.cs
@@ -5,22 +5,30 @@ using UnityEngine;
 public class Pieces : MonoBehaviour
 {
     public VRDragHandler dragHandler;
-    public GameObject[] pieces;
+    public Piece[] pieces;
     public float radius;
+    public float selectRadius;
     public float height;
-    public float initialRotation;
+    public float selectHeight;
     public float pieceScale;
+    public float selectPieceScale;
+    public float initialRotation;
 
     private int selection;
 
     private void Start()
     {
         var interval = 360 / pieces.Length;
+        pieces[0].onSelect.Invoke();
         for (var i = 0; i < pieces.Length; i++)
         {
-            pieces[i].transform.localScale = new Vector3(pieceScale, pieceScale, pieceScale);
-            var pos = Quaternion.Euler(0, (initialRotation + interval * i) % 360, 0) * new Vector3(radius, height, 0);
-            pieces[i].transform.position = pos;
+            var piece = pieces[i].transform;
+            piece.localScale = new Vector3(pieceScale, pieceScale, pieceScale);
+            var pos = Quaternion.Euler(0, (initialRotation + interval * i) % 360, 0) *
+                      new Vector3(i == 0 ? selectRadius : radius, i == 0 ? selectHeight : height, 0);
+            piece.position = pos;
+            var scale = i == 0 ? selectPieceScale : pieceScale;
+            piece.localScale = new Vector3(scale, scale, scale);
         }
 
         dragHandler.onDrag += OnDrag;
@@ -30,7 +38,7 @@ public class Pieces : MonoBehaviour
     {
         if (!dragHandler.swipeMode) return;
         var prev = selection;
-        selection = (selection + (int)value + pieces.Length) % pieces.Length;
+        selection = (selection - (int)value + pieces.Length) % pieces.Length;
         StartCoroutine(RotateCoroutine(prev, selection));
     }
 
@@ -38,17 +46,40 @@ public class Pieces : MonoBehaviour
     {
         var time = Time.time;
         var interval = 360f / pieces.Length;
-        var prevAngle = interval * prev;
-        var nextAngle = interval * next;
+        var prevAngle = -interval * prev;
+        var nextAngle = -interval * next;
         while (Time.time - time <= dragHandler.swipeCooldown * 0.9f)
         {
             yield return null;
-            var cur = initialRotation + Mathf.LerpAngle(prevAngle, nextAngle,
-                Math.Min(1, (Time.time - time) / (dragHandler.swipeCooldown * 0.9f)));
+            var progress = (Time.time - time) / (dragHandler.swipeCooldown * 0.9f);
+            var cur = initialRotation + Mathf.LerpAngle(prevAngle, nextAngle, Math.Min(1, progress));
             for (var i = 0; i < pieces.Length; i++)
             {
-                var pos = Quaternion.Euler(0, (cur + interval * i) % 360, 0) * new Vector3(radius, height, 0);
+                float x, y, s;
+                if (i == prev)
+                {
+                    pieces[i].onDeselect.Invoke();
+                    x = Mathf.Lerp(selectRadius, radius, progress);
+                    y = Mathf.Lerp(selectHeight, height, progress);
+                    s = Mathf.Lerp(selectPieceScale, pieceScale, progress);
+                }
+                else if (i == next)
+                {
+                    pieces[i].onSelect.Invoke();
+                    x = Mathf.Lerp(radius, selectRadius, progress);
+                    y = Mathf.Lerp(height, selectHeight, progress);
+                    s = Mathf.Lerp(pieceScale, selectPieceScale, progress);
+                }
+                else
+                {
+                    x = radius;
+                    y = height;
+                    s = pieceScale;
+                }
+
+                var pos = Quaternion.Euler(0, (cur + interval * i) % 360, 0) * new Vector3(x, y, 0);
                 pieces[i].transform.position = pos;
+                pieces[i].transform.localScale = new Vector3(s, s, s);
             }
         }
     }
