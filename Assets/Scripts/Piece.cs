8a7924030dd7c614bcc9e3619e8652ddc03217eb
younggam <nainjoo2001@naver.com>
2023-06-23 18:31:32 +0900

    import leaves

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -6,4 +7,27 @@ public class Piece : MonoBehaviour
     public UnityEvent onSelect;
     public UnityEvent onDeselect;
     public UnityEvent onClicked;
+
+    public MeshRenderer[] toPlayRenderers;
+    private readonly List<Material> toPlayMaterials = new();
+    private static int playProperty = Shader.PropertyToID("_Play");
+
+    private void Start()
+    {
+        for (var i = 0; i < toPlayRenderers.Length; i++)
+        {
+            var materials = toPlayRenderers[i].sharedMaterials;
+            for (var j = 0; j < materials.Length; j++) toPlayMaterials.Add(materials[j]);
+        }
+    }
+
+    public void PlayMaterials()
+    {
+        for (var i = 0; i < toPlayMaterials.Count; i++) toPlayMaterials[i].SetInt(playProperty, 1);
+    }
+
+    public void StopMaterials()
+    {
+        for (var i = 0; i < toPlayMaterials.Count; i++) toPlayMaterials[i].SetInt(playProperty, 0);
+    }
 }
--- Assets/Scripts/Pieces.cs
+++ Assets/Scripts/Pieces.cs
@@ -26,9 +26,11 @@ public class Pieces : MonoBehaviour
         {
             var piece = pieces[i].transform;
             piece.localScale = new Vector3(pieceScale, pieceScale, pieceScale);
-            var pos = Quaternion.Euler(0, (initialRotation + interval * i) % 360, 0) *
-                      new Vector3(i == 0 ? selectRadius : radius, i == 0 ? selectHeight : height, 0);
+            var yRot = Quaternion.Euler(0, (initialRotation + interval * i) % 360, 0);
+
+            var pos = yRot * new Vector3(i == 0 ? selectRadius : radius, i == 0 ? selectHeight : height, 0);
             piece.position = pos;
+            piece.rotation = yRot;
             var scale = i == 0 ? selectPieceScale : pieceScale;
             piece.localScale = new Vector3(scale, scale, scale);
         }
@@ -50,24 +52,28 @@ public class Pieces : MonoBehaviour
         var interval = 360f / pieces.Length;
         var prevAngle = -interval * prev;
         var nextAngle = -interval * next;
-        while (Time.time - time <= dragHandler.swipeCooldown * 0.9f)
+        var first = true;
+        var elapsed = 0f;
+        var threshold = dragHandler.swipeCooldown * 0.9f;
+        while (elapsed <= threshold)
         {
             yield return null;
-            var progress = (Time.time - time) / (dragHandler.swipeCooldown * 0.9f);
+            elapsed = Time.time - time;
+            var progress = elapsed / threshold;
             var cur = initialRotation + Mathf.LerpAngle(prevAngle, nextAngle, Math.Min(1, progress));
             for (var i = 0; i < pieces.Length; i++)
             {
                 float x, y, s;
                 if (i == prev)
                 {
-                    pieces[i].onDeselect.Invoke();
+                    if (first) pieces[i].onDeselect.Invoke();
                     x = Mathf.Lerp(selectRadius, radius, progress);
                     y = Mathf.Lerp(selectHeight, height, progress);
                     s = Mathf.Lerp(selectPieceScale, pieceScale, progress);
                 }
                 else if (i == next)
                 {
-                    pieces[i].onSelect.Invoke();
+                    if (elapsed > threshold) pieces[i].onSelect.Invoke();
                     x = Mathf.Lerp(radius, selectRadius, progress);
                     y = Mathf.Lerp(height, selectHeight, progress);
                     s = Mathf.Lerp(pieceScale, selectPieceScale, progress);
@@ -79,10 +85,15 @@ public class Pieces : MonoBehaviour
                     s = pieceScale;
                 }
 
-                var pos = Quaternion.Euler(0, (cur + interval * i) % 360, 0) * new Vector3(x, y, 0);
-                pieces[i].transform.position = pos;
-                pieces[i].transform.localScale = new Vector3(s, s, s);
+                var piece = pieces[i].transform;
+                var yRot = Quaternion.Euler(0, (cur + interval * i) % 360, 0);
+                var pos = yRot * new Vector3(x, y, 0);
+                piece.position = pos;
+                piece.rotation = yRot;
+                piece.localScale = new Vector3(s, s, s);
             }
+
+            first = false;
         }
     }
 }
